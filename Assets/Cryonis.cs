428263cd60d60e595cf8175caab4c579e2ddfc4b
AJ <jumperthejumper@gmail.com>
2020-11-02 19:06:37 -0600

    init

---
+++ Assets/Cryonis.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cryonis : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            Boost();
+
+        }
+    }
+
+    void Boost()
+    {
+        Debug.Log("boost");
+        float y = this.gameObject.transform.localScale.y;
+        float x = this.gameObject.transform.localScale.x;
+        float z = this.gameObject.transform.localScale.z;
+        transform.localScale = new Vector3(x, y * Time.deltaTime, z);
+    }
+}
---
+++ Assets/ListeningSkill.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListeningSkill : MonoBehaviour
+{
+    public GameObject ListeningCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            Debug.Log("now listening for enemy");
+            ListeningCanvas.SetActive(true);
+
+        }
+        else
+        {
+            ListeningCanvas.SetActive(false);
+        }
+    }
+}
